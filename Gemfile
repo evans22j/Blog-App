410304946de1eb65203d9896c3b16d4e750e36ee
Evans Sitibekiso <jevacrism@gmail.com>
2022-07-29 20:49:16 +0200

    update required changes in specs folder

--- Gemfile
+++ Gemfile
@@ -12,7 +12,7 @@ gem 'rails', '~> 7.0.3'
 gem 'sprockets-rails'
 
 # Use postgresql as the database for Active Record
-gem 'pg', '~> 1.1'
+gem 'pg'
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem 'puma', '~> 5.0'
@@ -50,10 +50,11 @@ gem 'bootsnap', require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
-gem 'ffi', github: 'ffi/ffi', submodules: true
+gem 'ffi'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
+  gem 'rails-controller-testing'
   gem 'rspec-rails'
 end
 
--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -1,31 +1,42 @@
 require 'rails_helper'
 
-RSpec.describe 'PostsController', type: :request do
-  describe 'GET /index' do
-    context 'when the page is loaded' do
-      it 'returns a 200 status code and render template for index' do
-        get '/users/1/posts'
-        expect(response).to have_http_status(200)
-        expect(response).to render_template(:index)
-      end
-      it 'includes pagination' do
-        get '/users/1/posts'
-        expect(response.body).to include('Pagination')
-      end
+RSpec.describe Post, type: :model do
+  describe 'For the Post model' do
+    before(:each) do
+      @user = User.new(name: 'John', bio: 'Teacher from Dubai', posts_counter: 0)
+      @post = Post.new(author: @user, title: 'Test', text: 'testing', likes_counter: 7, comments_counter: 5)
     end
-  end
 
-  describe 'GET /show' do
-    context 'when the page is loaded' do
-      it 'returns a 200 status code and render template for show' do
-        get '/users/1/posts', params: { id: 1 }
-        expect(response).to have_http_status(200)
-        expect(response).to render_template(:index)
-      end
-      it 'returns a 200 status code and render template for show' do
-        get '/users/1/posts', params: { id: 1 }
-        expect(response.body).to include('post')
-      end
+    before { @post.save }
+
+    it 'if there is title' do
+      @post.title = true
+      expect(@post).to be_valid
+    end
+
+    it 'if there is max 250 characters' do
+      @post.title = 'Testing'
+      expect(@post).to be_valid
+    end
+
+    it 'if likes counter is integer' do
+      @post.likes_counter = 5
+      expect(@post).to be_valid
+    end
+
+    it 'if likes counter greater than or equal to zero' do
+      @post.likes_counter = -9
+      expect(@post).to_not be_valid
+    end
+
+    it 'if comments counter greater than or equal to zero.' do
+      @post.comments_counter = -5
+      expect(@post).to_not be_valid
+    end
+
+    it 'if comments counter is integer' do
+      @post.comments_counter = 8
+      expect(@post).to be_valid
     end
   end
 end
