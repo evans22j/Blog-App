bc892690f6a0b401ac74748b3bd92dcac31cc9a6
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-08-04 23:12:06 +0300

    Add Redirects To User Index

--- spec/integration/user_index_spec.rb
+++ spec/integration/user_index_spec.rb
@@ -31,11 +31,12 @@ RSpec.describe 'Test Index Page', type: :feature do
       expect(page).to have_content('3')
       expect(page).to have_content('5')
     end
-    
+
     it 'redirects to users show page' do
       visit root_path
-      click_on @first_user.name
-      expect(page).to have_content(`#{@username1}\'s Most Recent Posts`)
+      click_link @first_user.name
+      expect(page).to have_content(@first_user.name)
+      expect(page).to have_content(@first_user.bio)
     end
   end
 end
